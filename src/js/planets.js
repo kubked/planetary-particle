4b697b36177c5f939749b6ba6420040ee899b5b4
Jakub Kędzierski <kubked@gmail.com>
2014-05-07 13:47:52 +0200

    Inner solar system

--- src/js/planets.js
+++ src/js/planets.js
@@ -23,28 +23,28 @@ var planets = {
 		period: 1.8808476,
 		color: "#A16D4D"
 	},
-	jupiter: {
-		radius: 69911.0,
-		starDistance: 778412010.0,
-		period: 11.862615,
-		color: "#D6CAC3"
-	},
-	saturn: {
-		radius: 58232.0,
-		starDistance: 1426725400.0,
-		period: 29.447498,
-		color: "#A9B391"
-	},
-	uranus: {
-		radius: 25362.0,
-		starDistance: 2870972200.0,
-		period: 84.016846,
-		color: "#8DF2D9"
-	},
-	neptune: {
-		radius: 24622.0,
-		starDistance: 4498252900.0,
-		period: 164.79132,
-		color: "#729DE8"
-	}
+	// jupiter: {
+	// 	radius: 69911.0,
+	// 	starDistance: 778412010.0,
+	// 	period: 11.862615,
+	// 	color: "#D6CAC3"
+	// },
+	// saturn: {
+	// 	radius: 58232.0,
+	// 	starDistance: 1426725400.0,
+	// 	period: 29.447498,
+	// 	color: "#A9B391"
+	// },
+	// uranus: {
+	// 	radius: 25362.0,
+	// 	starDistance: 2870972200.0,
+	// 	period: 84.016846,
+	// 	color: "#8DF2D9"
+	// },
+	// neptune: {
+	// 	radius: 24622.0,
+	// 	starDistance: 4498252900.0,
+	// 	period: 164.79132,
+	// 	color: "#729DE8"
+	// }
 };
